f232850f322c3f88b4b09f6fcf3874a738c80236
Piera254 <perisnjeri841@gmail.com>
2025-07-17 22:25:32 +0300

    Trial files

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("Hello world!")
+print("RESTART ME!!********")
+print("****i AM Just a Coder!!****")
+print("**********-------------*****")
+print("****This could be much better***")
+
+for i in range(1, 5):
+ print ("This is a fine lady")
+
+print("***********Goodbye*******************")
+
